761049dfb0a6969f203c0f07864e04d42868f77e
0703kyj <kyj0703@konkuk.ac.kr>
2024-08-26 22:44:07 +0900

    feat: 키, 몸무게 추가

--- src/main/kotlin/com/fancychild/bapsanghead/dto/response/UserResponse.kt
+++ src/main/kotlin/com/fancychild/bapsanghead/dto/response/UserResponse.kt
@@ -3,7 +3,9 @@ package com.fancychild.bapsanghead.dto.response
 data class UserResponse(
         val name: String,
         val email: String,
-        val age: String,
+        val age: Int,
+        val weight: Int,
+        val height: Int,
         val gender: String,
         val activityLevel: String,
 )
